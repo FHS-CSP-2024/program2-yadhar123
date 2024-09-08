63a9d78c39667553d7f28336f7251548dda5ca74
yadhar123 <yadhar16@gmail.com>
2024-09-08 16:54:40 +0000

    finished

--- Program2.py
+++ Program2.py
@@ -51,7 +51,9 @@
 # - Asks for the user's name and then prints it twice, on two consecutive lines.
 
 
-
+name = input("what is your name?")
+print (name)
+print (name)
 
 
 ## Problem 2 ##
@@ -60,6 +62,8 @@
 # - Prints it out twice on a single line so that there is an exclamation mark at the beginning of the line, 
 # - another between the two names and a third one at the end of the line.
 
+name = input ("what is your name?")
+print("!" + name + "!" + name + "!")
 
 ## Problem 3 ##
 #Please write a script that: 
@@ -71,16 +75,32 @@
 #   - Street address: 91 Station Road
 #   - City and postal code: Folsom CA, 95630
 
+firstname = input("what is your name?")
+lastname = input("what is your first name?")
+streetaddress = input("what is your street adress")
+postcode = input("what is your city and postal code?")
+print ("Here is all your info together :)")
+print (firstname)
+print (lastname)
+print (streetaddress)
+print (postcode)
+
+
 ## Problem 4 ##
 #Please write a script that: 
 # - Asks for 3 words 
 # - Puts the words together with dashes and prints that out
 
+word1= input("Enter word 1")
+word2= input("Enter word 2")
+word3= input("Enter word 3")
+
+print (word1 + "_" +word2+"_" + word3)
 
 ## Problem 5 ##
 #Please write a script that: 
 # - Asks for a name and a year
-# - Prints out a short story that uses that information
+# - Prints out a short story that uses thQQQQat information
 # Sample output:
 #Please type in a name: Mary
 #Please type in a year: 1572
@@ -88,3 +108,9 @@
 #Mary is a valiant knight, born in the year 1572. 
 #One morning Mary woke up to an awful racket: a dragon was approaching the village. 
 #Only Mary could save the village's residents.
+
+name= input("enter a anme for your very own fictional character:")
+year = input("what year do you want him/her to be in? ENter a future year here: ")
+print (name +"is a very magical person, born in the year" +year +".")
+print("In a world filled with rainbows and unicorns " + name + "was known for their magical powers")
+print("one day in the year"+ year+ "," +name+ "discovered a shrek that could run and only one person knew how to solve it, it was" +name+"Knew how to find whats right.")
